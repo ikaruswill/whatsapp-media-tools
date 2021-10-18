e5cb296361e71e069d8e0a5baa589a4ced172c3c
Will Ho <ikaruswill.ho@gmail.com>
2021-10-18 09:35:21 +0800

    Lint all python files

--- find-duplicates.py
+++ find-duplicates.py
@@ -30,8 +30,10 @@ def get_hash(filename, chunk_size, first_chunk_only=False, hash=hashlib.sha1):
 
 
 def check_for_duplicates(path, chunk_size, recursive, hash=hashlib.sha1):
-    hashes_by_size = defaultdict(list)  # dict of size_in_bytes: [full_path_to_file1, full_path_to_file2, ]
-    hashes_on_1k = defaultdict(list)  # dict of (hash1k, size_in_bytes): [full_path_to_file1, full_path_to_file2, ]
+    # dict of size_in_bytes: [full_path_to_file1, full_path_to_file2, ]
+    hashes_by_size = defaultdict(list)
+    # dict of (hash1k, size_in_bytes): [full_path_to_file1, full_path_to_file2, ]
+    hashes_on_1k = defaultdict(list)
     hashes_full = {}   # dict of full_file_hash: full_path_to_file_string
     duplicates = defaultdict(set)
 
@@ -45,23 +47,25 @@ def check_for_duplicates(path, chunk_size, recursive, hash=hashlib.sha1):
                     file_size = os.path.getsize(full_path)
                     hashes_by_size[file_size].append(full_path)
             except (OSError,):
-                    # not accessible (permissions, etc) - pass on
-                    logger.warning(f'Error reading file: {os.path.join(dirpath, filename)}')
-                    continue
-    else: 
+                # not accessible (permissions, etc) - pass on
+                logger.warning(
+                    f'Error reading file: {os.path.join(dirpath, filename)}')
+                continue
+    else:
         for dirpath, dirnames, filenames in os.walk(path):
             # get all files that have the same size - they are the collision candidates
             for filename in filenames:
                 full_path = os.path.join(dirpath, filename)
                 try:
-                    # if the target is a symlink (soft one), this will 
+                    # if the target is a symlink (soft one), this will
                     # dereference it - change the value to the actual target file
                     full_path = os.path.realpath(full_path)
                     file_size = os.path.getsize(full_path)
                     hashes_by_size[file_size].append(full_path)
                 except (OSError,):
                     # not accessible (permissions, etc) - pass on
-                    logger.warning(f'Error reading file: {os.path.join(dirpath, filename)}')
+                    logger.warning(
+                        f'Error reading file: {os.path.join(dirpath, filename)}')
                     continue
 
     logger.info('Comparing short hashes')
@@ -73,14 +77,16 @@ def check_for_duplicates(path, chunk_size, recursive, hash=hashlib.sha1):
 
         for filename in files:
             try:
-                small_hash = get_hash(filename, chunk_size, first_chunk_only=True)
+                small_hash = get_hash(
+                    filename, chunk_size, first_chunk_only=True)
                 # the key is the hash on the first chunk plus the size - to
                 # avoid collisions on equal hashes in the first part of the file
                 # credits to @Futal for the optimization
                 hashes_on_1k[(small_hash, size_in_bytes)].append(filename)
             except (OSError,):
-                # the file access might've changed till the exec point got here 
-                logger.warning(f'Error reading file: {os.path.join(path, filename)}')
+                # the file access might've changed till the exec point got here
+                logger.warning(
+                    f'Error reading file: {os.path.join(path, filename)}')
                 continue
 
     logger.info('Comparing full hashes')
@@ -91,18 +97,22 @@ def check_for_duplicates(path, chunk_size, recursive, hash=hashlib.sha1):
             continue    # this hash of fist 1k file bytes is unique, no need to spend CPU cycles on it
 
         for filename in files_list:
-            try: 
-                full_hash = get_hash(filename, chunk_size, first_chunk_only=False)
+            try:
+                full_hash = get_hash(filename, chunk_size,
+                                     first_chunk_only=False)
                 duplicate = hashes_full.get(full_hash)
                 if duplicate:
-                    logger.info(f'Duplicate: {os.path.basename(duplicate)} | {os.path.basename(filename)}')
+                    logger.info(
+                        f'Duplicate: {os.path.basename(duplicate)} | {os.path.basename(filename)}')
                     duplicates[duplicate].add(filename)
                 else:
                     hashes_full[full_hash] = filename
             except (OSError,):
-                logger.warning(f'Error reading file: {os.path.join(dirpath, filename)}')
+                logger.warning(
+                    f'Error reading file: {os.path.join(dirpath, filename)}')
                 continue
     return duplicates
+
 
 def single_yes_or_no_question(question, default_no=True):
     choices = ' [y/N]: ' if default_no else ' [Y/n]: '
@@ -117,7 +127,8 @@ def single_yes_or_no_question(question, default_no=True):
 
 
 def find_keep_file(files):
-    filename_files_dict = {os.path.splitext(os.path.basename(f))[0]: i for i, f in enumerate(files)}
+    filename_files_dict = {os.path.splitext(os.path.basename(f))[
+        0]: i for i, f in enumerate(files)}
     filenames = list(filename_files_dict.keys())
     filenames.sort()
     filenames.sort(key=len)
@@ -127,7 +138,8 @@ def find_keep_file(files):
 
 def main(path, chunk_size, recursive, force, dry_run):
     duplicates = check_for_duplicates(path, chunk_size, recursive)
-    logger.info(f'Number of duplicates found: {sum([len(files_set) for __, files_set in duplicates.items()])}')
+    logger.info(
+        f'Number of duplicates found: {sum([len(files_set) for __, files_set in duplicates.items()])}')
 
     if len(duplicates) == 0:
         logger.info('No duplicates found. Exiting.')
@@ -138,7 +150,7 @@ def main(path, chunk_size, recursive, force, dry_run):
 
     if not force:
         delete = single_yes_or_no_question('Delete duplicates?')
-    
+
     if delete or force:
         for key_file, files_set in duplicates.items():
             files_set.add(key_file)
@@ -153,17 +165,24 @@ def main(path, chunk_size, recursive, force, dry_run):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        'WhatsApp duplicate file remover', 
-        description='Remove duplicated media, preserving the file with the shortest filename or earliest date encoded in the filename.')
+        description=('Remove duplicated media, preserving the file with the shortest filename'
+                     'or earliest date encoded in the filename.'))
     parser.add_argument('path', type=str, help='Path to WhatsApp media folder')
-    parser.add_argument('-c', '--chunk-size', default=1024, type=int, help='Chunk size for heuristic, smaller values are generally faster but if many files have identical starting chunks, performance degrades as more full hashes are computed')
-    parser.add_argument('-f', '--force', action='store_true', help='Delete duplicates without prompting')
-    parser.add_argument('-r', '--recursive', default=False, action='store_true', help='Recursively process media')
-    parser.add_argument('--dry-run', default=False, action='store_true', help='Dry run deletion (no files deleted)')
+    parser.add_argument('-c', '--chunk-size', default=1024, type=int,
+                        help=('Chunk size for heuristic, smaller values are generally faster'
+                              'but if many files have identical starting chunks, performance'
+                              'degrades as more full hashes are computed'))
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='Delete duplicates without prompting')
+    parser.add_argument('-r', '--recursive', default=False,
+                        action='store_true', help='Recursively process media')
+    parser.add_argument('--dry-run', default=False, action='store_true',
+                        help='Dry run deletion (no files deleted)')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
     logger = logging.getLogger('find-duplicates')
-    
-    main(args.path, args.chunk_size, args.recursive, force=args.force, dry_run=args.dry_run)
 
+    main(args.path, args.chunk_size, args.recursive,
+         force=args.force, dry_run=args.dry_run)
--- restore-exif.py
+++ restore-exif.py
@@ -14,26 +14,31 @@ def get_datetime(filename):
     date_str = filename.split('-')[1]
     return datetime.strptime(date_str, '%Y%m%d')
 
+
 def get_exif_datestr(filename):
     return get_datetime(filename).strftime("%Y:%m:%d %H:%M:%S")
+
 
 def get_filepaths(path, recursive):
     all_filepaths = []
     if not recursive:
         abspath = os.path.abspath(path)
-        all_filepaths += [(abspath, f) for f in os.listdir(abspath) if os.path.isfile(os.path.join(abspath, f))]
+        all_filepaths += [(abspath, f) for f in os.listdir(abspath)
+                          if os.path.isfile(os.path.join(abspath, f))]
     else:
         for dirpath, dirnames, filenames in os.walk(path):
             abspath = os.path.abspath(dirpath)
             all_filepaths += [(abspath, f) for f in filenames]
     return all_filepaths
 
+
 def filter_filepaths(filepaths, allowed_ext):
     return [(fp, fn) for fp, fn in filepaths if os.path.splitext(fn)[-1] in allowed_ext]
 
 
 def make_new_exif(filename):
-    exif_dict = {'Exif': {piexif.ExifIFD.DateTimeOriginal: get_exif_datestr(filename)}}
+    exif_dict = {
+        'Exif': {piexif.ExifIFD.DateTimeOriginal: get_exif_datestr(filename)}}
     return piexif.dump(exif_dict)
 
 
@@ -43,7 +48,7 @@ def is_whatsapp_img(filename):
 
 def is_whatsapp_vid(filename):
     return bool(vid_filename_regex.match(filename))
-    
+
 
 def main(path, recursive, mod):
     logger.info('Validating arguments')
@@ -52,12 +57,12 @@ def main(path, recursive, mod):
 
     if not os.path.isdir(path):
         raise TypeError('Path specified is not a directory')
-    
+
     logger.info('Listing files in target directory')
     filepaths = get_filepaths(path, recursive)
     logger.info(f'Total files: {len(filepaths)}')
 
-    allowed_extensions = set(['.mp4','.jpg','.3gp','.jpeg'])
+    allowed_extensions = set(['.mp4', '.jpg', '.3gp', '.jpeg'])
     logger.info(f'Filtering for valid file extensions: {allowed_extensions}')
     filepaths = filter_filepaths(filepaths, allowed_ext=allowed_extensions)
     num_files = len(filepaths)
@@ -69,7 +74,8 @@ def main(path, recursive, mod):
     abspath_len = len(abspath) + 1
     for i, (path, filename) in enumerate(filepaths):
         filepath = os.path.join(path, filename)
-        logger.info(f'{i + 1:>{progress_digits}}/{num_files} - {filepath[abspath_len:]}')
+        logger.info(
+            f'{i + 1:>{progress_digits}}/{num_files} - {filepath[abspath_len:]}')
         if filename.endswith('.mp4') or filename.endswith('.3gp'):
             if not is_whatsapp_vid(filename):
                 logger.warning('File is not a valid WhatsApp video, skipping')
@@ -82,14 +88,15 @@ def main(path, recursive, mod):
             if not is_whatsapp_img(filename):
                 logger.warning('File is not a valid WhatsApp image, skipping')
                 continue
-        
+
             try:
                 exif_dict = piexif.load(filepath)
                 if exif_dict['Exif'].get(piexif.ExifIFD.DateTimeOriginal):
                     logger.info('Exif date already exists, skipping')
                     continue
-            
-                exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = get_exif_datestr(filename)
+
+                exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = get_exif_datestr(
+                    filename)
                 exif_bytes = piexif.dump(exif_dict)
             except piexif.InvalidImageDataError:
                 logger.warning(f'Invalid image data, skipping')
@@ -108,15 +115,16 @@ def main(path, recursive, mod):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        'WhatsApp Exif Date Restore', 
         description='Restore discarded Exif date information in WhatsApp media based on the filename.')
     parser.add_argument('path', type=str, help='Path to WhatsApp media folder')
-    parser.add_argument('-r', '--recursive', default=False, action='store_true', help='Recursively process media')
-    parser.add_argument('-m', '--mod', default=False, action='store_true', help='Set file modified date')
+    parser.add_argument('-r', '--recursive', default=False,
+                        action='store_true', help='Recursively process media')
+    parser.add_argument('-m', '--mod', default=False,
+                        action='store_true', help='Set file modified date')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
     logger = logging.getLogger('restore-exif')
-    
-    main(args.path, recursive=args.recursive, mod=args.mod)
 
+    main(args.path, recursive=args.recursive, mod=args.mod)
